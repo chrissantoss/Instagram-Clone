af1bc5b3b8de9b8f30c79a2da20222e58b9d41de
Christopher Santos <chrissantos221@gmail.com>
2020-10-10 19:49:38 -0400

    First commit

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h1>Lets build an ig clone!</h1> */}
+    
+        {/* Header */}
+        <div className="app__header">
+          <img
+            className="app__headerImage"
+            src={'https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png'}
+            alt=""
+          />
+        </div>
+
+        <h1>Lets build an ig clone!</h1>
+        {/* Posts */}
+        {/* Posts */}
+    </div>
+
+  );
+}
+
+export default App;
